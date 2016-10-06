a83ab11854709db4c3ea6d39f801458ec6944e8a
Serguei Outenkov <sergueioutenkov@gmail.com>
2016-10-05 23:51:17 -0300

    Clase 4 ejemplo de FAB

--- app/src/main/java/com/serguei/cursos/cursoandroidcme/MainActivity.java
+++ app/src/main/java/com/serguei/cursos/cursoandroidcme/MainActivity.java
@@ -1,7 +1,11 @@
 package com.serguei.cursos.cursoandroidcme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "My First Snackbar!", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("MainActivity", "Clicked!");
+                            }
+                        }).show();
+            }
+        });
     }
 }
